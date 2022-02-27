fe2bbc75f52d247f469aaa0b7629de69bacdfe12
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2022-02-27 09:53:17 +0200

    Update BaseViewModel.kt

--- app/src/main/java/com/hk/mvipractice/commons/presentation/BaseViewModel.kt
+++ app/src/main/java/com/hk/mvipractice/commons/presentation/BaseViewModel.kt
@@ -1,8 +1,10 @@
 package com.hk.mvipractice.commons.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 /**
  * Created by hassankhamis on 22,February,2022
@@ -11,8 +13,12 @@ abstract class BaseViewModel <State: UiState, Event: UiEvent, Effect: UiEffect>:
 
     private val initialState : State by lazy { createInitialState() }
 
-    private val _state = MutableStateFlow<UiState>(initialState)
+    private val _state = MutableStateFlow<State>(initialState)
     val state get() = _state.asStateFlow()
+
+    // Get Current State
+    val currentState: State
+        get() = state.value
 
     private val _event = MutableSharedFlow<Event>()
     val event get() = _event.asSharedFlow()
@@ -20,6 +26,51 @@ abstract class BaseViewModel <State: UiState, Event: UiEvent, Effect: UiEffect>:
     private val _effect = Channel<Effect>()
     val effect get() = _effect.receiveAsFlow()
 
-    abstract fun createInitialState(): State
+    init {
+        subscribeEvents()
+    }
 
+    /**
+     * Start listening to Event
+     */
+    private fun subscribeEvents() {
+        viewModelScope.launch {
+            event.collect {
+                handleEvent(it)
+            }
+        }
+    }
+
+    /**
+     * Set new Event
+     */
+
+    fun setEvent(event: Event) {
+        viewModelScope.launch {
+            _event.emit(event)
+        }
+    }
+
+    /**
+     * Set new Ui State
+     */
+
+    protected fun setState(reduce: State.() -> State) {
+        viewModelScope.launch {
+            val newState = currentState.reduce()
+            _state.value = newState
+        }
+    }
+
+    protected fun setEffect(builder: () -> Effect) {
+        val effect = builder()
+        viewModelScope.launch { _effect.send(effect) }
+    }
+
+    /**
+     * Handle each event
+     */
+    abstract fun handleEvent(event : Event)
+
+    abstract fun createInitialState(): State
 }
