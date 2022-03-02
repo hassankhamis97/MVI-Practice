1efe9c26843589626ef427ddb639d3410c0e0847
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2022-03-02 08:56:07 +0200

    Create HomeViewModel.kt

---
+++ app/src/main/java/com/hk/mvipractice/modules/home/presentation/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.hk.mvipractice.modules.home.presentation
+
+import com.hk.mvipractice.commons.presentation.BaseViewModel
+
+/**
+ * Created by hassankhamis on 23,February,2022
+ */
+//class HomeViewModel: BaseViewModel<>() {
+//}
